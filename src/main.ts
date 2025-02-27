3060b6f77061a1fa6bd0bcf3fff70590b7ea8d69
Egor <104233247+RR3Z@users.noreply.github.com>
2025-02-27 20:28:27 +0300

    update: connect World with Scene

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import Graphic from "./engine/graphic.ts"
 import Interface from "./engine/gui.ts"
 import Light from "./engine/light.ts"
 import { loadDices } from "./engine/loader.ts"
+import World from "./engine/world.ts"
 
 export const availableDices: Record<string, Object3D> = await loadDices(
 	"models/dices/rpg_dice_set.gltf"
@@ -13,7 +14,9 @@ export let selectedDices: Object3D[] = []
 const scene = new Scene()
 const camera = new Camera()
 const light = new Light()
+export const world = new World()
 
+scene.add(world)
 scene.add(light)
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement
