5d22c9424a76d1bf300c14b57a6acb8ffd4896b2
Egor <104233247+RR3Z@users.noreply.github.com>
2025-02-27 10:56:28 +0300

    add: can get viewport sizes

---
+++ src/DiceRoller/utils.ts
@@ -0,0 +1,11 @@
+import { PerspectiveCamera } from "three"
+
+export function getViewportSizes(camera: PerspectiveCamera) {
+	const distance = Math.abs(camera.position.y) // Расстояние от камеры до объекта
+	const fov = camera.fov * (Math.PI / 180) // Преобразуем FOV в радианы
+	const cameraHeight = 2 * Math.tan(fov / 2) * distance // Высота видимой области
+	const aspectRatio = window.innerWidth / window.innerHeight
+	const cameraWidth = cameraHeight * aspectRatio
+
+	return { width: cameraWidth, height: cameraHeight }
+}
