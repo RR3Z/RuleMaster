da26fe0abf771c6728ce909646a46da5d5ed7fd7
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-01 13:28:55 +0300

    add: connect diceBox to physic world

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import Camera from "./engine/visual/camera.ts"
 import Graphic from "./engine/visual/graphic.ts"
 import Light from "./engine/visual/light.ts"
 import { loadDices } from "./engine/visual/loader.ts"
+import DiceBox from "./entity/diceBox.ts"
 import World from "./entity/world.ts"
 import Interface from "./gui.ts"
 
@@ -13,6 +14,9 @@ export const availableDices: Record<string, Object3D> = await loadDices(
 	"models/dices/rpg_dice_set.gltf"
 )
 export let selectedDices: Object3D[] = []
+
+// Dice Box
+const diceBox = new DiceBox()
 
 // Visual
 export const scene = new Scene() // TODO: Remove 'export' in the end
@@ -26,6 +30,7 @@ export const world = new World(physicWorld)
 
 scene.add(world)
 scene.add(light)
+diceBox.create(physicWorld, camera)
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement
 const graphic = new Graphic(scene, camera, canvas)
