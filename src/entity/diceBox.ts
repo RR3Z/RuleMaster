112589a91642898ad85a4b8f3714f5152056c6ae
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-01 13:28:31 +0300

    add: dicebox

---
+++ src/entity/diceBox.ts
@@ -0,0 +1,74 @@
+import Rapier from "@dimforge/rapier3d-compat"
+import { getViewportSizes } from "../engine/utils.ts"
+import Camera from "../engine/visual/camera.ts"
+
+export default class DiceBox {
+	private wallsPhysic: {
+		type: string
+		rigidBody: Rapier.RigidBody
+		collider: Rapier.Collider
+	}[] = []
+
+	constructor() {}
+
+	public create(physicalWorld: Rapier.World, camera: Camera): void {
+		const { width, height } = getViewportSizes(camera)
+
+		const wallsConfig = [
+			{
+				type: "floor",
+				position: [0, 0, 0],
+				size: [width / 2, 0.1, height / 2],
+			},
+			{
+				type: "roof",
+				position: [0, 30, 0],
+				size: [width / 2, 0.1, height / 2],
+			},
+			{
+				type: "left",
+				position: [-width / 2 + 5, 15, 0],
+				size: [0.1, 15, height / 2],
+			},
+			{
+				type: "right",
+				position: [width / 2 - 5, 15, 0],
+				size: [0.1, 15, height / 2],
+			},
+			{
+				type: "back",
+				position: [0, 15, -height / 2 + 5],
+				size: [width / 2, height / 2, 0.1],
+			},
+			{
+				type: "front",
+				position: [0, 15, height / 2 - 5],
+				size: [width / 2, height / 2, 0.1],
+			},
+		]
+
+		wallsConfig.forEach(wallConfig => {
+			const rigidBodyDesc = Rapier.RigidBodyDesc.fixed()
+			// Position
+			rigidBodyDesc.setTranslation(
+				wallConfig.position[0],
+				wallConfig.position[1],
+				wallConfig.position[2]
+			)
+
+			const rigidBody = physicalWorld.createRigidBody(rigidBodyDesc)
+			const colliderDesc = Rapier.ColliderDesc.cuboid(
+				wallConfig.size[0],
+				wallConfig.size[1],
+				wallConfig.size[2]
+			)
+			const collider = physicalWorld.createCollider(colliderDesc, rigidBody)
+
+			this.wallsPhysic.push({
+				type: wallConfig.type,
+				rigidBody: rigidBody,
+				collider: collider,
+			})
+		})
+	}
+}
