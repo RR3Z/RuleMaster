7f66608436b8756b7a0ac0680dc7ca33f32c0b7a
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-05 21:19:32 +0300

    update: rewrite everything using the DiceRoller class

--- src/gui.ts
+++ src/gui.ts
@@ -1,59 +1,57 @@
 import GUI from "lil-gui"
+import DiceRoller from "./diceRoller.ts"
 import Dice, { DiceType } from "./entity/dice.ts"
-import {
-	availableDices,
-	physicDebugger,
-	scene,
-	selectedDices,
-	world,
-} from "./main.ts"
 
 // TODO: REMOVE IT IN THE END - TEMP SOLUTION
 export default class Interface extends GUI {
+	private diceRoller: DiceRoller
+
 	private guiElements = {
-		addD4: function () {
-			selectedDices.push(new Dice(availableDices["D4"].clone(), DiceType.D4))
+		addD4: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D4"].clone(), DiceType.D4)
+			)
 		},
-		addD6: function () {
-			selectedDices.push(new Dice(availableDices["D6"].clone(), DiceType.D6))
+		addD6: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D6"].clone(), DiceType.D6)
+			)
 		},
-		addD8: function () {
-			selectedDices.push(new Dice(availableDices["D8"].clone(), DiceType.D8))
+		addD8: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D8"].clone(), DiceType.D8)
+			)
 		},
-		addD10: function () {
-			selectedDices.push(new Dice(availableDices["D10"].clone(), DiceType.D10))
+		addD10: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D10"].clone(), DiceType.D10)
+			)
 		},
-		addD12: function () {
-			selectedDices.push(new Dice(availableDices["D12"].clone(), DiceType.D12))
+		addD12: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D12"].clone(), DiceType.D12)
+			)
 		},
-		addD20: function () {
-			selectedDices.push(new Dice(availableDices["D20"].clone(), DiceType.D20))
-		},
-		addDices: function () {
-			scene.add(physicDebugger.debugMesh)
-			world.addDices(selectedDices)
-		},
-		clearScene: function () {
-			scene.remove(physicDebugger.debugMesh)
-			world.removeDices(selectedDices)
-			selectedDices.splice(0, selectedDices.length)
+		addD20: () => {
+			this.diceRoller.selectedDices.push(
+				new Dice(this.diceRoller.dicesVisual["D20"].clone(), DiceType.D20)
+			)
 		},
 	}
 
-	constructor() {
+	constructor(diceRoller: DiceRoller) {
 		super()
+		this.diceRoller = diceRoller
 		this.init()
 	}
 
 	private init(): void {
-		this.add(physicDebugger, "isEnabled").name("Physic Debugger Status")
+		this.add(this.diceRoller, "switchState").name("Switch State (Dice Roller)")
 		this.add(this.guiElements, "addD4").name("D4")
 		this.add(this.guiElements, "addD6").name("D6")
 		this.add(this.guiElements, "addD8").name("D8")
 		this.add(this.guiElements, "addD10").name("D10")
 		this.add(this.guiElements, "addD12").name("D12")
 		this.add(this.guiElements, "addD20").name("D20")
-		this.add(this.guiElements, "addDices").name("Add Dices")
-		this.add(this.guiElements, "clearScene").name("Clear Scene")
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,7 @@
-import { Object3D, Scene } from "three"
-import { physicWorld } from "./engine/physic/physic.ts"
-import PhysicDebugger from "./engine/physic/physicDebugger.ts"
-import Camera from "./engine/visual/camera.ts"
-import Graphic from "./engine/visual/graphic.ts"
-import Light from "./engine/visual/light.ts"
-import { loadDices } from "./engine/visual/loader.ts"
-import Dice from "./entity/dice.ts"
-import DiceBox from "./entity/diceBox.ts"
-import World from "./entity/world.ts"
+import DiceRoller from "./diceRoller.ts"
 import Interface from "./gui.ts"
 
-// Dices
-export const availableDices: Record<string, Object3D> = await loadDices(
-	"models/dices/dices.gltf"
-)
-export let selectedDices: Dice[] = []
+export const diceRoller = new DiceRoller()
+await diceRoller.loadDices("models/dices/dices.gltf")
 
-// Dice Box
-const diceBox = new DiceBox()
-
-// Visual
-export const scene = new Scene() // TODO: Remove 'export' in the end
-const camera = new Camera()
-const light = new Light()
-// Physic
-export const physicDebugger = new PhysicDebugger(physicWorld, scene) // TODO: Remove it in the end
-
-// Objects with Physic and Visual
-export const world = new World(physicWorld)
-
-scene.add(world)
-scene.add(light)
-diceBox.create(physicWorld, camera)
-
-const canvas = document.querySelector("canvas") as HTMLCanvasElement
-const graphic = new Graphic(scene, camera, canvas)
-
-// Если надо добавить новую логику при обновлении кадров, добавляем сюда
-graphic.onUpdate(() => {
-	physicWorld.step()
-	world.update()
-	physicDebugger.update()
-})
-
-// TODO: REMOVE IT IN THE END - TEMP SOLUTION
-const gui = new Interface()
+const gui = new Interface(diceRoller) // GUI - temp solution TODO: remove it in the end
