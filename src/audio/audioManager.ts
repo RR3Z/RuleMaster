1817dd58f2681f979e3f2eca3ae136fdcb729934
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-07 13:18:25 +0300

    refactor: change file path

--- src/audio/audioManager.ts
+++ src/audio/audioManager.ts
@@ -1,7 +1,7 @@
 export default class AudioManager {
 	constructor() {}
 
-	public playSound(audioFile: string = "/public/audio/dice-hit.mp3"): void {
+	public playSound(audioFile: string = "/audio/dice-hit.mp3"): void {
 		const sound = new Audio(audioFile)
 		sound.play()
 	}
