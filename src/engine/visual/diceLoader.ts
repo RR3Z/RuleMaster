288a939db8ec2b6200b14c1507d71c395dfbabb0
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-07 12:41:48 +0300

    update: now DiceLoader inherited from GLTFLoader

--- src/engine/visual/diceLoader.ts
+++ src/engine/visual/diceLoader.ts
@@ -1,14 +1,15 @@
 import { Object3D } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 
-export default class DiceLoader {
-	constructor() {}
+export default class DiceLoader extends GLTFLoader {
+	constructor() {
+		super()
+	}
 
 	public async load(filePath: string): Promise<Record<string, Object3D>> {
-		const loader = new GLTFLoader()
 		const dices: Record<string, Object3D> = {}
 
-		const gltf = await loader.loadAsync(filePath)
+		const gltf = await this.loadAsync(filePath)
 		for (const dice of gltf.scene.children) {
 			dices[dice.name] = dice
 		}
