d111e38af9f787e172b51a6f6b3e73007eeed97e
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-09 19:21:04 +0300

    update: enable InteractiveMap canvas display

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import DiceRoller from "./DiceRoller/diceRoller.ts"
 import Interface from "./DiceRoller/gui.ts"
+import InteractiveMap from "./InteractiveMap/interactiveMap.ts"
 
-export const diceRoller = new DiceRoller()
+// Dice Roller
+export const diceRoller = new DiceRoller() // TODO: remove "export" in the end
 await diceRoller.loadDices("models/dices/dices.gltf")
-
 const gui = new Interface(diceRoller) // GUI - temp solution TODO: remove it in the end
+
+// Interactive Map
+const interactiveMap = new InteractiveMap()
+await interactiveMap.init()
+interactiveMap.onUpdate(() => {})
