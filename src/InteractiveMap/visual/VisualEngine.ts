de73d3d9bcfd2b05e70c76fc957860cf48b82e9c
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-12 22:56:43 +0300

    update: add Camera to VisualEngine

--- src/InteractiveMap/visual/VisualEngine.ts
+++ src/InteractiveMap/visual/VisualEngine.ts
@@ -4,29 +4,33 @@ import Camera from './Camera.ts'
 import GridVisual from './GridVisual.ts'
 
 export default class VisualEngine extends Application {
-	private _scene: Container
+	private _scene!: Container
 	private _camera!: Camera
 	private callbackUpdate!: () => void
 
 	constructor() {
 		super()
-
-		this._scene = new Container()
 	}
 
-	public get scene(): Container {
-		return this._scene
-	}
-
-	public async initialize(grid: Grid): Promise<void> {
-		await this.init({ resizeTo: window })
-		this._camera = new Camera(this.stage, this.canvas)
-
+	public async init(grid: Grid): Promise<void> {
+		// Pixi.Js Initialization
+		await super.init({ resizeTo: window })
 		this.canvas.id = 'interactiveMap'
 
-		this.scene.addChild(new GridVisual(grid))
-		this.stage.addChild(this.scene)
+		// Objects Initialization
+		this._camera = new Camera(this.renderer, {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		})
+		this._scene = new Container()
 
+		// Scene Setup
+		this.stage.addChild(this._camera)
+		this._camera.addChild(this._scene) // Make objects move with camera
+		this._scene.addChild(new GridVisual(grid))
+		this._camera.updateSettings()
+
+		// Start Update Loop
 		this.startLoop()
 	}
 
