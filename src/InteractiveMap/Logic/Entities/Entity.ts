a6e943452fde302a0953393f625236a549de2eb0
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-17 13:30:46 +0300

    add: Entity class

--- src/InteractiveMap/Logic/Entities/Entity.ts
+++ src/InteractiveMap/Logic/Entities/Entity.ts
@@ -1,3 +1,9 @@
-export default class Entity {
-	constructor() {}
+import { EntityType } from './EntityType.ts'
+
+export default abstract class Entity {
+	protected _type!: EntityType
+
+	public get type(): EntityType {
+		return this._type
+	}
 }
---
+++ src/InteractiveMap/Logic/Entities/EntityType.ts
@@ -0,0 +1,5 @@
+export enum EntityType {
+	CHARACTER,
+	ENEMY,
+	BOUNDARY,
+}
