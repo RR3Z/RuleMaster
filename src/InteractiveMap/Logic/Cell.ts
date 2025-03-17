7a164d762206b5c7e89ffe618a836d8c027d9019
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-17 13:30:31 +0300

    add: now can put and pull content from Cell

--- src/InteractiveMap/Logic/Cell.ts
+++ src/InteractiveMap/Logic/Cell.ts
@@ -5,7 +5,7 @@ export type Coordinates = { x: number; y: number }
 export default class Cell {
 	private _x: number
 	private _y: number
-	public _content: Entity | null = null
+	private _content: Entity | undefined = undefined
 
 	constructor(x: number, y: number, content?: Entity) {
 		this._x = x
@@ -19,5 +19,32 @@ export default class Cell {
 
 	public get y(): number {
 		return this._y
+	}
+
+	public putContent(content: Entity): void {
+		if (this._content)
+			throw new Error(
+				'Trying put content to cell when it is not empty! Position: ' +
+					this._x +
+					' ' +
+					this._y
+			)
+
+		this._content = content
+	}
+
+	public pullContent(): Entity | undefined {
+		if (this._content === undefined)
+			throw new Error(
+				'Trying get content from cell when it is empty! Position: ' +
+					this._x +
+					' ' +
+					this._y
+			)
+
+
+		const content = this._content
+		this._content = undefined
+		return content
 	}
 }
