118e6334c39f12dfe76bc78a4b0a33db89d1cc21
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-17 12:18:52 +0300

    update: now cell can store content

--- src/InteractiveMap/Logic/Cell.ts
+++ src/InteractiveMap/Logic/Cell.ts
@@ -1,14 +1,16 @@
+import Entity from './Entities/Entity.ts'
+
 export type Coordinates = { x: number; y: number }
 
 export default class Cell {
 	private _x: number
 	private _y: number
-	public content: any
+	public _content: Entity | null = null
 
-	constructor(x: number, y: number, content?: any) {
+	constructor(x: number, y: number, content?: Entity) {
 		this._x = x
 		this._y = y
-		this.content = content
+		if (content) this._content = content
 	}
 
 	public get x(): number {
