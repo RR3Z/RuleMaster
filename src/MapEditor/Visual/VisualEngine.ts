3748b8f422af4db7c39d977af6f03bc83aa70f6e
Egor <104233247+RR3Z@users.noreply.github.com>
2025-03-21 11:28:49 +0300

    update: now can load backgrounds for maps

--- src/MapEditor/Visual/VisualEngine.ts
+++ src/MapEditor/Visual/VisualEngine.ts
@@ -1,4 +1,4 @@
-import { Application, Container } from 'pixi.js'
+import { Application, Assets, Container, Sprite } from 'pixi.js'
 import Camera from '../../InteractiveMap/Visual/Camera.ts'
 import { MapData } from '../MapEditor.ts'
 import MapEditorGUI from '../MapEditorGUI.ts'
@@ -23,19 +23,30 @@ export default class VisualEngine extends Application {
 		this._scene = new Container()
 	}
 
-	public setupScene(editor: MapEditorGUI, mapData: MapData): void {
+	public async setupScene(
+		editor: MapEditorGUI,
+		mapData: MapData
+	): Promise<void> {
 		// Scene Setup
 		if (this._camera.children.length > 0) this._camera.removeChild(this._scene)
 		this._scene = new Container()
 		this._camera.addChild(this._scene) // Make objects move with camera
 
-		// if (mapData.mapFilePath) {
-		// 	let mapBackgroundPromise = Assets.load(mapData.mapFilePath)
-		// 	mapBackgroundPromise.then(asset => {
-		// 		const sprite = new Sprite(asset)
-		// 		this._scene.addChild(sprite)
-		// 	})
-		// }
+		if (mapData.mapFilePath) {
+			let mapBackground = await Assets.load(mapData.mapFilePath)
+			const backgroundSprite = new Sprite(mapBackground)
+			backgroundSprite.scale.x =
+				(mapData.grid.width * mapData.grid.cellSize) /
+				backgroundSprite.texture.width
+			backgroundSprite.scale.y =
+				(mapData.grid.height * mapData.grid.cellSize) /
+				backgroundSprite.texture.height
+			backgroundSprite.anchor.set(0, 0)
+			backgroundSprite.position.set(0, 0)
+
+			this._scene.addChild(backgroundSprite)
+			console.log(this._scene.getBounds())
+		}
 		this._scene.addChild(new GridVisual(editor, mapData.grid, mapData.cells))
 
 		this._camera.updateSettings()
