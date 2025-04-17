b9297e0bd7705d8a64ab2bb0fe5f808d38e7bf8d
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-17 10:10:20 +0300

    add: new methods for Cell

--- src/InteractiveMap/Model/Map/Cell.ts
+++ src/InteractiveMap/Model/Map/Cell.ts
@@ -1,4 +1,4 @@
-import Entity from '../Entities/Entity'
+import Entity, { EntityType } from '../Entities/Entity'
 
 export interface CellData {
 	x: number
@@ -15,5 +15,37 @@ export default class Cell {
 		this._x = data.x
 		this._y = data.y
 		this._content = data.content
+	}
+
+	public get x(): number {
+		return this._x
+	}
+
+	public get y(): number {
+		return this._y
+	}
+
+	public get contentType(): EntityType | undefined {
+		return this._content?.type
+	}
+
+	public putContent(content: Entity) {
+		if (this._content !== undefined)
+			throw new Error(
+				`Cell (${this._x}, ${this._y}): has already content (${this._content}): `
+			)
+
+		this._content = content
+	}
+
+	public pullContent(): Entity | undefined {
+		if (this._content === undefined)
+			throw new Error(`Cell (${this._x}, ${this._y}): has no content `)
+
+		if (this._content.type === EntityType.BOUNDARY) return undefined
+
+		const content = this._content
+		this._content = undefined
+		return content
 	}
 }
