413c0d23aab240fc4cb7ab1a8f4c7c664e8a8af3
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-20 13:37:59 +0300

    update: Token architecture

---
+++ src/InteractiveMap/View/Visual/Characters/CharacterToken.ts
@@ -0,0 +1,72 @@
+import { Container, FederatedPointerEvent } from 'pixi.js'
+import { Subject } from 'rxjs'
+import { Position, TokenVisualData } from '../../../_Types/Characters.ts'
+import Character from '../../../Model/Entities/Characters/Character.ts'
+import VisualUtils from '../../../VisualUtils.ts'
+import Token from './Token.ts'
+
+export type CharacterPosition = {
+	character: Character
+	pos: Position
+}
+
+export default class CharacterToken extends Token {
+	// Fields
+	private _character: Character
+	// Events
+	public positionChanged$: Subject<CharacterPosition>
+
+	constructor(
+		data: TokenVisualData,
+		parent: Container,
+		cellSize: number,
+		character: Character
+	) {
+		super()
+
+		this._parent = parent // TODO: зачем мне хранить ссылку на своего parent, если такое уже есть в Graphics? (Если это только не разные объекты)
+		this._isDragging = false
+		this._radius = cellSize / 2
+		this._character = character
+		this.positionChanged$ = new Subject<CharacterPosition>()
+
+		this.init(data)
+
+		this.on('pointerdown', this.onDragStart, this)
+		this.on('pointerup', this.onDragEnd, this)
+		this.on('pointerupoutside', this.onDragEnd, this)
+	}
+
+	private onDragStart(event: FederatedPointerEvent): void {
+		if (event.button === 0) {
+			this.alpha = 0.5
+			this._isDragging = true
+			this._parent.on('pointermove', this.onDragMove, this)
+		}
+	}
+
+	private onDragMove(event: FederatedPointerEvent): void {
+		if (this._isDragging) {
+			// Convert global position to local
+			this.parent.toLocal(event.global, undefined, this.position)
+			// Snap Token to Cell
+			this.position = VisualUtils.snapToCell(
+				this.position.x,
+				this.position.y,
+				this.getBounds().width / 2
+			)
+		}
+	}
+
+	private onDragEnd(event: FederatedPointerEvent): void {
+		if (this._isDragging) {
+			this._parent.off('pointermove', this.onDragMove, this)
+			this.alpha = 1
+			this._isDragging = false
+			this.positionChanged$.next({
+				pos: { x: this.position.x, y: this.position.y },
+				character: this._character,
+			})
+		}
+	}
+}
--- src/InteractiveMap/View/Visual/Characters/Token.ts
+++ src/InteractiveMap/View/Visual/Characters/Token.ts
@@ -1,24 +1,14 @@
-import { Container, FederatedPointerEvent, Graphics, Rectangle } from 'pixi.js'
-import { CharacterVisualData } from '../../../_Types/Characters.ts'
+import { Container, Graphics, Rectangle } from 'pixi.js'
+import { TokenVisualData } from '../../../_Types/Characters.ts'
 import VisualUtils from '../../../VisualUtils.ts'
 
-export default class Token extends Graphics {
-	private _parent: Container
-	private _isDragging: boolean
-	private _radius: number
+export default abstract class Token extends Graphics {
+	// Fields
+	protected _parent!: Container
+	protected _isDragging!: boolean
+	protected _radius!: number
 
-	constructor(data: CharacterVisualData, parent: Container, cellSize: number) {
-		super()
-
-		this._parent = parent
-		this._isDragging = false
-		this._radius = cellSize / 2
-
-		this.init(data)
-		this.subscribe()
-	}
-
-	private init(data: CharacterVisualData): void {
+	protected init(data: TokenVisualData): void {
 		const pos = VisualUtils.coordinatesToPixelPosition(
 			data.x,
 			data.y,
@@ -38,40 +28,5 @@ export default class Token extends Graphics {
 
 		this.eventMode = 'static'
 		this.cursor = 'pointer'
-	}
-
-	private subscribe(): void {
-		this.on('pointerdown', this.onDragStart, this)
-		this.on('pointerup', this.onDragEnd, this)
-		this.on('pointerupoutside', this.onDragEnd, this)
-	}
-
-	private onDragStart(event: FederatedPointerEvent): void {
-		if (event.button === 0) {
-			this.alpha = 0.5
-			this._isDragging = true
-			this._parent.on('pointermove', this.onDragMove, this)
-		}
-	}
-
-	private onDragMove(event: FederatedPointerEvent): void {
-		if (this._isDragging) {
-			// Convert global position to local
-			this.parent.toLocal(event.global, undefined, this.position)
-			// Snap Token to Cell
-			this.position = VisualUtils.snapToCell(
-				this.position.x,
-				this.position.y,
-				this.getBounds().width / 2
-			)
-		}
-	}
-
-	private onDragEnd(event: FederatedPointerEvent): void {
-		if (this._isDragging) {
-			this._parent.off('pointermove', this.onDragMove, this)
-			this.alpha = 1
-			this._isDragging = false
-		}
 	}
 }
