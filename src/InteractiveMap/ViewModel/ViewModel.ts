08ff6b74ebc11b885070297f460445668c269072
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-20 13:44:04 +0300

    add: handler for characters positionChanged event

--- src/InteractiveMap/ViewModel/ViewModel.ts
+++ src/InteractiveMap/ViewModel/ViewModel.ts
@@ -1,9 +1,15 @@
 import GameModel from '../Model/GameModel.ts'
+import { CharacterPosition } from '../View/Visual/Characters/CharacterToken.ts'
 
 export default class ViewModel {
 	private _model: GameModel
 
 	constructor(model: GameModel) {
 		this._model = model
+	}
+
+	// Handlers
+	public onCharacterPositionChanged(data: CharacterPosition): void {
+		this._model.moveCharacterTo(data.character, data.pos)
 	}
 }
