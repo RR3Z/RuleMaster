2029dc8d4941f15226abbbde059551ee00e19a70
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-20 13:40:05 +0300

    add: new fields in GameModel

--- src/InteractiveMap/Model/GameModel.ts
+++ src/InteractiveMap/Model/GameModel.ts
@@ -1,13 +1,27 @@
-import { MapLogicData } from '../_Types/Map.ts'
+import { EnemyData } from '../_Types/Characters.ts'
+import { MapLogicData, Position } from '../_Types/Map.ts'
+import Character from './Entities/Characters/Character.ts'
+import Enemy from './Entities/Characters/Enemy.ts'
+import Player from './Entities/Characters/Player.ts'
 import AStarPathFinder from './Map/AStarPathFinder/AStarPathFinder.ts'
 import Grid from './Map/Grid.ts'
 
 export default class GameModel {
-	private _grid: Grid
+	// Fields
+	public readonly player: Player
+	public readonly enemies: Set<Enemy>
 	private _pathFinder: AStarPathFinder
+	private _grid: Grid
 
 	constructor(data: MapLogicData) {
-		this._grid = new Grid(data.grid)
 		this._pathFinder = new AStarPathFinder()
+		this._grid = new Grid(data.grid)
+		this.player = new Player(data.player)
+
+		this.enemies = new Set<Enemy>()
+		data.enemies.forEach((data: EnemyData) => {
+			this.enemies.add(new Enemy(data))
+		})
+	}
 	}
 }
