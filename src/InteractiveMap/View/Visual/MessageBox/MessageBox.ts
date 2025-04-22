93d4060e8cb805d7df2adcc481b4c01f82607d1d
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-22 15:18:35 +0300

    add: Overlay and use display instead of visibility

--- src/InteractiveMap/View/Visual/MessageBox/MessageBox.ts
+++ src/InteractiveMap/View/Visual/MessageBox/MessageBox.ts
@@ -5,31 +5,34 @@ export default class MessageBox {
 	private _message: HTMLSpanElement
 	private _closeButton: HTMLButtonElement
 	private _nextButton: HTMLButtonElement
+	private _overlay: HTMLDivElement
 
 	constructor() {
-		// Create Elements
-		this._container = document.body.appendChild(document.createElement('div'))
-		this._container.id = 'tutorialMessageBox'
-
-		this._message = document.createElement('span')
-		this._container.appendChild(this._message)
-		this._message.id = 'tutorialText'
-
-		this._closeButton = document.createElement('button')
-		this._container.appendChild(this._closeButton)
-		this._closeButton.id = 'messageBoxCloseBtn'
-
-		this._nextButton = document.createElement('button')
-		this._container.appendChild(this._nextButton)
-		this._nextButton.id = 'messageBoxNextBtn'
+		this._container = document.body.querySelector(
+			'#tutorialMessageBox'
+		) as HTMLDivElement
+		this._message = this._container.querySelector(
+			'#tutorialText'
+		) as HTMLSpanElement
+		this._closeButton = this._container.querySelector(
+			'#messageBoxCloseBtn'
+		) as HTMLButtonElement
+		this._nextButton = this._container.querySelector(
+			'#messageBoxNextBtn'
+		) as HTMLButtonElement
+		this._overlay = document.body.querySelector('#overlay') as HTMLDivElement
 
 		// Style Elements
-		this._container.style.visibility = 'hidden'
-		this._container.style.backgroundColor = '#ffffff'
+		this._container.style.display = 'none'
+		this._overlay.style.display = 'none'
 
 		// Events
-		this._closeButton.addEventListener('click', this.switchVisibility)
-		this._nextButton.addEventListener('click', this.switchVisibility)
+		this._closeButton.addEventListener('click', () => {
+			this.switchVisibility()
+		})
+		this._nextButton.addEventListener('click', () => {
+			this.switchVisibility()
+		})
 	}
 
 	public get closeButton(): Readonly<HTMLButtonElement> {
@@ -41,24 +44,27 @@ export default class MessageBox {
 	}
 
 	public updateData(data: MessageBoxData): void {
-		console.log(data)
 		if (data === undefined) return
 
 		this._message.textContent = data.message
 		if (data.needNext) {
-			this._closeButton.style.visibility = 'hidden'
-			this._nextButton.style.visibility = 'visible'
+			this._closeButton.style.display = 'none'
+			this._nextButton.style.display = 'flex'
 		} else {
-			this._closeButton.style.visibility = 'visible'
-			this._nextButton.style.visibility = 'hidden'
+			this._closeButton.style.display = 'flex'
+			this._nextButton.style.display = 'none'
 		}
 
 		this.switchVisibility()
 	}
 
 	private switchVisibility(): void {
-		if (this._container.style.visibility === 'hidden')
-			this._container.style.visibility = 'visible'
-		else this._container.style.visibility = 'hidden'
+		if (this._container.style.display === 'none') {
+			this._container.style.display = 'flex'
+			this._overlay.style.display = 'flex'
+		} else {
+			this._container.style.display = 'none'
+			this._overlay.style.display = 'none'
+		}
 	}
 }
