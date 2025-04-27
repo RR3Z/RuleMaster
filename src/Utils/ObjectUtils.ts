1bcee3c2b1249e49e2fd9d2875ee1da9fb7ccaca
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-27 12:29:14 +0300

    add: deep objects data comparison for two objects

---
+++ src/Utils/ObjectUtils.ts
@@ -0,0 +1,35 @@
+export function areObjectDataEqual(a: object, b: object): boolean {
+	if (
+		a === null ||
+		b === null ||
+		typeof a !== 'object' ||
+		typeof b !== 'object'
+	)
+		return false
+
+	if (a === b) return true
+
+	const keysA = Object.keys(a)
+	const keysB = Object.keys(b)
+	if (keysA.length !== keysB.length) return false
+
+	for (const key of keysA) {
+		if (!keysB.includes(key)) return false
+
+		const valA = a[key as keyof typeof a]
+		const valB = b[key as keyof typeof b]
+
+		if (
+			typeof valA === 'object' &&
+			valA !== null &&
+			typeof valB === 'object' &&
+			valB !== null
+		) {
+			if (!areObjectDataEqual(valA, valB)) return false
+		} else if (valA !== valB) {
+			return false
+		}
+	}
+
+	return true
+}
