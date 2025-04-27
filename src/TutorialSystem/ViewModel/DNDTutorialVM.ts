f321bdade24ee2ae0a74de176c2a895b8cdc83fa
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-27 18:31:52 +0300

    add: some logic to DNDTutorialVM

--- src/TutorialSystem/ViewModel/DNDTutorialVM.ts
+++ src/TutorialSystem/ViewModel/DNDTutorialVM.ts
@@ -1,10 +1,28 @@
+import { Observable, Subject } from 'rxjs'
+import { TutorialStep } from '../Steps/TutorialStep.ts'
 import TutorialModel from '../TutorialModel.ts'
 import TutorialVM from './TutorialVM.ts'
 
 export default class DNDTutorialVM extends TutorialVM {
+	private _currentStep$ = new Subject<TutorialStep>()
+
 	constructor(model: TutorialModel) {
 		super()
 
 		this._model = model
+		this._currentStep$ = new Subject<TutorialStep>()
+
+		// Model Events Subscriptions
+		this._model.currentStep$.subscribe((stepData: TutorialStep) => {
+			this._currentStep$.next(stepData)
+		})
+	}
+
+	public get currentStep$(): Observable<TutorialStep> {
+		return this._currentStep$.asObservable()
+	}
+
+	public dispose(): void {
+		this._currentStep$.complete()
 	}
 }
