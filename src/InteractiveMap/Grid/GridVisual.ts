062e14c934cddeef0685aa71571d10381005e099
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-28 18:50:09 +0300

    add: GridVisual for InteractiveMap

---
+++ src/InteractiveMap/Grid/GridVisual.ts
@@ -0,0 +1,42 @@
+import { Container, Graphics } from 'pixi.js'
+import { GridOfCellsVisualData } from '../_Types/GridOfCellsVisualData'
+import { GridSizes } from '../_Types/GridSizes'
+
+export default class GridVisual extends Container {
+	// Settings
+	public isBoundariesEnabled: boolean = true
+
+	constructor(data: GridOfCellsVisualData, gridSizes: GridSizes) {
+		super()
+
+		this.draw(data, gridSizes)
+	}
+
+	private draw(data: GridOfCellsVisualData, gridSizes: GridSizes): void {
+		// Vertical lines
+		for (
+			let i = this.isBoundariesEnabled ? 0 : 1;
+			i < gridSizes.width + (this.isBoundariesEnabled ? 1 : 0);
+			i++
+		) {
+			const line = new Graphics()
+				.moveTo(i * data.cell.radius, 0)
+				.lineTo(i * data.cell.radius, data.cell.radius * gridSizes.height)
+				.stroke({ color: data.cell.color, pixelLine: true })
+			this.addChild(line)
+		}
+
+		// Horizontal lines
+		for (
+			let i = this.isBoundariesEnabled ? 0 : 1;
+			i < gridSizes.height + (this.isBoundariesEnabled ? 1 : 0);
+			i++
+		) {
+			const line = new Graphics()
+				.moveTo(0, i * data.cell.radius)
+				.lineTo(data.cell.radius * gridSizes.width, i * data.cell.radius)
+				.stroke({ color: data.cell.color, pixelLine: true })
+			this.addChild(line)
+		}
+	}
+}
