7d225ffc8b72ef0dc52c985f399f4f5c7bea5444
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-28 13:50:46 +0300

    refactor: Cell class

--- src/InteractiveMap/Grid/Cell.ts
+++ src/InteractiveMap/Grid/Cell.ts
@@ -1,48 +1,44 @@
+import { default as Entity } from '../Entities/Entity'
 import { EntityType } from '../Entities/EntityType'
-import IEntity from '../Entities/IEntity'
+import { Position } from '../Types/Position'
 
 export default class Cell {
-	private readonly _x: number
-	private readonly _y: number
-	private _content: IEntity | undefined
+	private readonly _pos: Position
+	private _content: Entity | null
 
-	constructor(x: number, y: number, content: IEntity | undefined = undefined) {
-		this._x = x
-		this._y = y
+	constructor(pos: Position, content: Entity | null = null) {
+		this._pos = pos
 		this._content = content
 	}
 
-	public get x(): number {
-		return this._x
-	}
-
-	public get y(): number {
-		return this._y
-	}
-
-	public get contentType(): EntityType | undefined {
+	public get contentType(): EntityType | null {
+		if (this._content === null) return null
 		return this._content?.type
 	}
 
-	public putContent(content: IEntity): void {
-		if (this._content !== undefined)
+	public get pos(): Readonly<Position> {
+		return this._pos
+	}
+
+	public putContent(content: Entity): void {
+		if (this._content !== null)
 			throw new Error(
-				`Cell (${this._x}, ${this._y}): already has content (${this._content.type})`
+				`Cell (${this._pos.x}, ${this._pos.y}): already has content (${this._content.type})`
 			)
 
 		this._content = content
 	}
 
-	public pullContent(): IEntity {
-		if (this._content === undefined)
-			throw new Error(`Cell (${this._x}, ${this._y}): has no content`)
+	public pullContent(): Entity {
+		if (this._content === null)
+			throw new Error(`Cell (${this._pos.x}, ${this._pos.y}): has no content`)
 		else if (this._content.type === EntityType.WALL)
 			throw new Error(
-				`Cell (${this._x}, ${this._y}): is Wall (you can't pull it)!`
+				`Cell (${this._pos.x}, ${this._pos.y}): is Wall (you can't pull it)!`
 			)
 
 		const content = this._content
-		this._content = undefined
+		this._content = null
 		return content
 	}
 }
