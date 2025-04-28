3e88b47bc7bf6d83b422369d30d3dc84d72ff8d9
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-28 18:49:19 +0300

    add: GridOfCellsView for MVVM

---
+++ src/InteractiveMap/View/MapView.ts
@@ -0,0 +1,11 @@
+import MapVM from '../ViewModel/MapVM'
+import MapVisualEngine from '../VisualEngine/MapVisualEngine'
+
+export default class MapView {
+	protected _viewModel: MapVM
+	protected _visualEngine!: MapVisualEngine
+
+	constructor(viewModel: MapVM) {
+		this._viewModel = viewModel
+	}
+}
