4d74ca57e33fa50ef5e49631acf6b751e09b0bb0
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-29 19:28:34 +0300

    refactor: remove constructor from abstract class MapModel

--- src/InteractiveMap/Model/MapModel.ts
+++ src/InteractiveMap/Model/MapModel.ts
@@ -1,11 +1,9 @@
+import { GameType } from '../_Types/GameType'
 import Player from '../Entities/Player'
 
 export default abstract class MapModel {
-	protected _player: Player
-
-	constructor(player: Player) {
-		this._player = player
-	}
+	protected _gameType!: GameType
+	protected _player!: Player
 
 	public get player(): Readonly<Player> {
 		return this._player
