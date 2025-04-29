585edaa2de003453c5b7d10336623ba52f6d6bf5
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-29 12:57:47 +0300

    add: InteractiveMapComponentsFabric class

---
+++ src/InteractiveMap/InteractiveMapComponentsFabric.ts
@@ -0,0 +1,36 @@
+import { MapData } from './_Types/MapData'
+import { MapType } from './_Types/MapType'
+import GridOfCellsModel from './Model/GridOfCellsModel'
+import MapModel from './Model/MapModel'
+import GridOfCellsView from './View/GridOfCellsView'
+import MapView from './View/MapView'
+import GridOfCellsVM from './ViewModel/GridOfCellsVM'
+import MapVM from './ViewModel/MapVM'
+
+export type InteractiveMapComponents = {
+	view: MapView
+	model: MapModel
+	viewModel: MapVM
+}
+
+export default class InteractiveMapComponentsFabric {
+	constructor() {}
+
+	public create(mapType: MapType, mapData: MapData): InteractiveMapComponents {
+		switch (mapType) {
+			case MapType.GRID_OF_CELLS:
+				const model = new GridOfCellsModel(mapData.logic)
+				const viewModel = new GridOfCellsVM(model)
+				const view = new GridOfCellsView({
+					data: mapData.visual,
+					viewModel: viewModel,
+					grid: model.grid,
+				})
+				return { model: model, viewModel: viewModel, view: view }
+			default:
+				throw new Error(
+					'InteractiveMapComponentsFabric -> create(): Unknown MapType'
+				)
+		}
+	}
+}
