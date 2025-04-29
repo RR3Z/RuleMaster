da85d35cefab04c8bb49e78a449b502e1d1a2789
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-29 19:30:10 +0300

    add: new class DNDCharacter

---
+++ src/InteractiveMap/Entities/Characters/DNDCharacter.ts
@@ -0,0 +1,18 @@
+import { CharacterLogicData } from '../../_Types/CharacterLogicData'
+import { DNDCharacterData } from '../../_Types/DNDCharacterData'
+import { EntityType } from '../EntityType'
+import Character from './Character'
+
+export default class DNDCharacter extends Character {
+	constructor(entityType: EntityType, data: CharacterLogicData) {
+		super(entityType, data.pos)
+
+		// General
+		this._name = data.name
+
+		// Specific
+		const dndCharacterData = data.data as DNDCharacterData
+		this._maxHealth = dndCharacterData.maxHealth
+		this._currentHealth = dndCharacterData.maxHealth
+	}
+}
