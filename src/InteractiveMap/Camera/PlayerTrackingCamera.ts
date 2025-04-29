6a2e9144aabb7772b43f7626bd6c86e96fa52268
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-29 14:30:44 +0300

    add: now PlayerTrackingCamera move with target

--- src/InteractiveMap/Camera/PlayerTrackingCamera.ts
+++ src/InteractiveMap/Camera/PlayerTrackingCamera.ts
@@ -2,7 +2,6 @@ import { Renderer } from 'pixi.js'
 import Token from '../Entities/Tokens/Token'
 import Camera from './Camera'
 
-// TODO:
 export default class PlayerTrackingCamera extends Camera {
 	private _target!: Token
 
@@ -12,5 +11,14 @@ export default class PlayerTrackingCamera extends Camera {
 
 	public set target(target: Token) {
 		this._target = target
+		this.moveCenter(this._target.x, this._target.y)
+		this.centerOnTarget()
+	}
+
+	private centerOnTarget(): void {
+		this.follow(this._target, {
+			speed: 5,
+			// radius: 200,
+		})
 	}
 }
