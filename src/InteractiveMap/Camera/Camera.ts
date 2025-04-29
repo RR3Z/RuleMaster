a18f98c90218fa43cd8c32dad9e1259ca26606aa
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-29 14:29:09 +0300

    update: remove plugins from Camera class

--- src/InteractiveMap/Camera/Camera.ts
+++ src/InteractiveMap/Camera/Camera.ts
@@ -1,9 +1,9 @@
-import { ClampZoom, Viewport } from 'pixi-viewport'
+import { Viewport } from 'pixi-viewport'
 import { Renderer } from 'pixi.js'
 
 export default abstract class Camera extends Viewport {
 	protected _zoomMinScale: number = 0.25
-	protected _zoomMaxScale: number = 1
+	protected _zoomMaxScale: number = 0.3
 
 	constructor(renderer: Renderer) {
 		super({
@@ -13,38 +13,7 @@ export default abstract class Camera extends Viewport {
 			disableOnContextMenu: true,
 		})
 
-		this.enablePlugins()
-
 		window.addEventListener('resize', () => this.onResize())
-	}
-
-	protected enablePlugins(): void {
-		this.wheel()
-		this.clampZoom({})
-
-		this.updatePlugins()
-	}
-
-	protected updatePlugins(): void {
-		this.updateWorldSizes()
-		this.updateClampZoom()
-	}
-
-	private updateWorldSizes(): void {
-		const bounds = this.getBounds()
-		this.worldHeight = bounds.maxY - bounds.minY
-		this.worldWidth = bounds.maxX - bounds.minX
-	}
-
-	private updateClampZoom(): void {
-		const clampZoom = this.plugins.get('clamp-zoom')! as ClampZoom
-
-		clampZoom.options.minHeight = this.worldHeight * this._zoomMinScale
-		clampZoom.options.minWidth = this.worldWidth * this._zoomMinScale
-		clampZoom.options.maxHeight = this.worldHeight * this._zoomMaxScale
-		clampZoom.options.maxWidth = this.worldWidth * this._zoomMaxScale
-
-		clampZoom.reset()
 	}
 
 	private onResize() {
