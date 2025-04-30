4c9a9f162fddf5b4a5b07e40e9bae0f1d686dacd
Egor <104233247+RR3Z@users.noreply.github.com>
2025-04-30 12:06:32 +0300

    refactor: remove useless files

--- src/InteractiveMap/Entities/Enemy.ts
+++
@@ -1,9 +0,0 @@
-import { Position } from '../_Types/Position'
-import Character from './Characters/Character'
-import { EntityType } from './EntityType'
-
-export default class Enemy extends Character {
-	constructor(pos: Position) {
-		super(EntityType.ENEMY, pos)
-	}
-}
--- src/InteractiveMap/Entities/Player.ts
+++
@@ -1,9 +0,0 @@
-import { DNDCharacterData } from '../_Types/DNDCharacterData'
-import Character from './Characters/Character'
-import { EntityType } from './EntityType'
-
-export default class Player extends Character {
-	constructor(data: DNDCharacterData) {
-		super(EntityType.PLAYER, data)
-	}
-}
