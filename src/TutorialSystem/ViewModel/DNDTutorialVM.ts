554defec7d1cd911f75730716951101403deeebd
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-04 15:39:55 +0300

    update: move events from DNDTutorialVM to TutorialVM

--- src/TutorialSystem/ViewModel/DNDTutorialVM.ts
+++ src/TutorialSystem/ViewModel/DNDTutorialVM.ts
@@ -1,13 +1,10 @@
-import { Observable, Subject } from 'rxjs'
+import { Subject } from 'rxjs'
 import { DNDTutorialStep } from '../Steps/DND/DNDTutorialStep.ts'
 import { TutorialStep } from '../Steps/TutorialStep.ts'
 import TutorialModel from '../TutorialModel.ts'
 import TutorialVM from './TutorialVM.ts'
 
 export default class DNDTutorialVM extends TutorialVM {
-	private _currentStep$ = new Subject<DNDTutorialStep>()
-	private _onTutorialEnd$ = new Subject<string[]>()
-
 	constructor(model: TutorialModel) {
 		super()
 
@@ -21,14 +18,6 @@ export default class DNDTutorialVM extends TutorialVM {
 		this._model.onTutorialEnd$.subscribe((onEndMessages: string[]) =>
 			this._onTutorialEnd$.next(onEndMessages)
 		)
-	}
-
-	public get currentStep$(): Observable<DNDTutorialStep> {
-		return this._currentStep$.asObservable()
-	}
-
-	public get onTutorialEnd$(): Observable<string[]> {
-		return this._onTutorialEnd$.asObservable()
 	}
 
 	public dispose(): void {
--- src/TutorialSystem/ViewModel/TutorialVM.ts
+++ src/TutorialSystem/ViewModel/TutorialVM.ts
@@ -1,5 +1,17 @@
+import { Observable, Subject } from 'rxjs'
+import { TutorialStep } from '../Steps/TutorialStep.ts'
 import TutorialModel from '../TutorialModel.ts'
 
 export default abstract class TutorialVM {
 	protected _model!: TutorialModel
+	protected _currentStep$!: Subject<TutorialStep>
+	protected _onTutorialEnd$!: Subject<string[]>
+
+	protected get currentStep$(): Observable<TutorialStep> {
+		return this._currentStep$.asObservable()
+	}
+
+	protected get onTutorialEnd$(): Observable<string[]> {
+		return this._onTutorialEnd$.asObservable()
+	}
 }
