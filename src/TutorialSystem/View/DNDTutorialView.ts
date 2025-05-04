539608ceeebfa3ed4fda3ca1d05f8a446657d0af
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-04 14:38:34 +0300

    update: add HTML elements to TutorialView and implement its abstract methods

--- src/TutorialSystem/View/DNDTutorialView.ts
+++ src/TutorialSystem/View/DNDTutorialView.ts
@@ -1,6 +1,10 @@
 import { DNDTutorialStep } from '../Steps/DND/DNDTutorialStep.ts'
 import { DNDUserActionType } from '../Steps/DND/DNDUserActionType.ts'
 import DNDTutorialVM from '../ViewModel/DNDTutorialVM.ts'
+import ActionsPanel from './Components/ActionsPanel.ts'
+import InteractiveMapCanvas from './Components/InteractiveMapCanvas.ts'
+import MessageBox from './Components/MessageBox.ts'
+import RightSideMenu from './Components/RightSideMenu.ts'
 import TutorialView from './TutorialView.ts'
 
 export default class DNDTutorialView extends TutorialView {
@@ -9,17 +13,43 @@ export default class DNDTutorialView extends TutorialView {
 
 		this._viewModel = viewModel
 
+		this._interactiveMapCanvas = new InteractiveMapCanvas()
+		this._actionsPanel = new ActionsPanel()
+		this._messageBox = new MessageBox()
+		this._rightSideMenu = new RightSideMenu()
+
 		// ViewModel Events Subscriptions
 		;(this._viewModel as DNDTutorialVM).currentStep$.subscribe(
 			(stepData: DNDTutorialStep) => {
-				// TODO: 1) Запретить все доступные действия
-				// TODO: 2) Разрешить перечисленные в allowedActions действия
-				// TODO: 3) Вывести все сообщения в MessageBox
+				this.disableAllActions()
+				this.enableActions(stepData.allowedActions)
+				this._messageBox.showMessages(stepData.messages)
 			}
 		)
 	}
 
-	// TODO: реализовать
-	private enableActions(allowedActions: DNDUserActionType[]): void {}
-	private disableActions(): void {}
+	protected enableActions(allowedActions: DNDUserActionType[]): void {
+		for (let action of allowedActions) {
+			switch (action) {
+				case DNDUserActionType.MOVE:
+					this._interactiveMapCanvas.enable()
+					console.log(
+						'Tutorial System View -> enableActions(): enable Interactive Map for MOVE'
+					)
+					break
+				default:
+					throw new Error(
+						'Tutorial System View -> enableActions(): unknown DNDUserActionType!'
+					)
+			}
+		}
+	}
+
+	protected disableAllActions(): void {
+		console.log('Tutorial System View -> disableAllActions()')
+
+		this._interactiveMapCanvas.disable()
+		this._rightSideMenu.disableDiceRollerTab()
+		this._actionsPanel.disable()
+	}
 }
--- src/TutorialSystem/View/TutorialView.ts
+++ src/TutorialSystem/View/TutorialView.ts
@@ -1,5 +1,17 @@
 import TutorialVM from '../ViewModel/TutorialVM.ts'
+import ActionsPanel from './Components/ActionsPanel.ts'
+import InteractiveMapCanvas from './Components/InteractiveMapCanvas.ts'
+import MessageBox from './Components/MessageBox.ts'
+import RightSideMenu from './Components/RightSideMenu.ts'
 
 export default abstract class TutorialView {
 	protected _viewModel!: TutorialVM
+
+	protected _interactiveMapCanvas!: InteractiveMapCanvas
+	protected _messageBox!: MessageBox
+	protected _rightSideMenu!: RightSideMenu
+	protected _actionsPanel!: ActionsPanel
+
+	protected abstract enableActions(...args: any[]): void
+	protected abstract disableAllActions(...args: any[]): void
 }
