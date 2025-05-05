102719d504cef287d1b014ff6cf901167d4a6ca7
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-05 21:54:17 +0300

    add: Actions Manager for DND

---
+++ src/InteractiveMap/ActionsManager/DNDActionsManager.ts
@@ -0,0 +1,37 @@
+import { ActionPhase } from '../_Types/ActionPhase'
+import CellsAStarPathFinder from '../AStarPathFinder/CellsAStarPathFinder'
+import Character from '../Entities/Characters/Character'
+import DNDMoveAction from './Actions/DNDActions/DNDMoveAction'
+import IPhasedAction from './Actions/IPhasedAction'
+import ActionsManager from './ActionsManager'
+
+export default class DNDActionsManager extends ActionsManager {
+	private _currentAction: IPhasedAction | null
+	private readonly _moveAction: DNDMoveAction
+
+	constructor(pathFinder: CellsAStarPathFinder) {
+		super()
+
+		this._currentAction = null
+		this._moveAction = new DNDMoveAction(pathFinder)
+	}
+
+	public get moveAction(): DNDMoveAction {
+		return this._moveAction
+	}
+
+	public perform(action: IPhasedAction, actor: Character, ...args: any): void {
+		if (
+			this._currentAction === null ||
+			this._currentAction.currentPhase() === ActionPhase.COMPLETE
+		) {
+			this._currentAction = action
+			this._currentAction.reset()
+		} else if (this._currentAction !== action)
+			throw new Error(
+				'DNDActionsManager -> perform(): Past action is not complete yet!'
+			)
+
+		this._currentAction.enterPhaseInput(actor, ...args)
+	}
+}
