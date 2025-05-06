eaa7006af75a051f37cba51020e4a966410ad11c
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-06 15:28:08 +0300

    add: WeaponDescriptors to WeaponData

--- src/InteractiveMap/_Types/WeaponData.ts
+++ src/InteractiveMap/_Types/WeaponData.ts
@@ -1,8 +1,10 @@
 import { DicesRollFormula } from '../../DiceRoller/_Types/DicesRollFormula'
+import { WeaponDescriptor } from './WeaponDescriptor'
 import { WeaponType } from './WeaponType'
 
 export type WeaponData = {
 	id: string // Must be ALWAYS 'weapon'
+	descriptors: WeaponDescriptor[]
 	name: string
 	type: WeaponType
 	description: string
---
+++ src/InteractiveMap/_Types/WeaponDescriptor.ts
@@ -0,0 +1,3 @@
+export enum WeaponDescriptor {
+	FENCING,
+}
