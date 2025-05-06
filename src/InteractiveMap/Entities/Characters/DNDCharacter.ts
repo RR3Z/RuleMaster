8aff7aa35264a32779ae4695f5c507d2912b8996
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-06 14:38:46 +0300

    add: defenceClass to DNDCharacter

--- src/InteractiveMap/Entities/Characters/DNDCharacter.ts
+++ src/InteractiveMap/Entities/Characters/DNDCharacter.ts
@@ -1,5 +1,7 @@
+import { ArmourData } from '../../_Types/ArmourData'
 import { CharacterLogicData } from '../../_Types/CharacterLogicData'
 import { DNDCharacterData } from '../../_Types/DNDCharacterData'
+import { SlotType } from '../../_Types/SlotType'
 import DNDEquipmentManager from '../../EquipmentManager/DNDEquipmentManager'
 import { EntityType } from '../EntityType'
 import Character from './Character'
@@ -8,6 +10,7 @@ import DNDCharacterStats from './DNDCharacterStats'
 export default class DNDCharacter extends Character {
 	private _maxMovementDistance: number
 	private _movementDistance: number
+	private _defenceClass: number
 	private _stats: DNDCharacterStats
 
 	constructor(entityType: EntityType, data: CharacterLogicData) {
@@ -28,6 +31,7 @@ export default class DNDCharacter extends Character {
 			new Map(dndCharacterData.stats),
 			dndCharacterData.proficiency
 		)
+		this._defenceClass = dndCharacterData.defenceClass
 	}
 
 	public get maxMovementDistance(): number {
@@ -36,6 +40,18 @@ export default class DNDCharacter extends Character {
 
 	public get movementDistance(): number {
 		return this._movementDistance
+	}
+
+	public get defenceClass(): number {
+		let selfValue = this._defenceClass
+		let armourValue = 0
+		if (this.equipmentManager.getItemData(SlotType.ARMOUR)) {
+			armourValue = (
+				this.equipmentManager.getItemData(SlotType.ARMOUR) as ArmourData
+			).armourClass
+		}
+
+		return armourValue > selfValue ? armourValue : selfValue
 	}
 
 	public get equipmentManager(): DNDEquipmentManager {
