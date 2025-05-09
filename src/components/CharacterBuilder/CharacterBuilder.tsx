ca2a6f2981ed0bdb6b06a0cdd8902b7c36e02dcf
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-09 17:55:22 +0300

    add: error messages

--- src/components/CharacterBuilder/CharacterBuilder.tsx
+++ src/components/CharacterBuilder/CharacterBuilder.tsx
@@ -5,7 +5,7 @@ import { BuilderData } from '@/types/CharacterBuilder/BuilderData'
 import { BuilderStep } from '@/types/CharacterBuilder/BuilderStep'
 import { DNDClass } from '@/types/CharacterBuilder/DNDClass'
 import { WelcomeData } from '@/types/CharacterBuilder/Steps/WelcomeData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import CharacterName from './CharacterName'
 import ClassesList from './Classes/ClassesList/ClassesList'
@@ -39,21 +39,55 @@ export const DEFAULT_NAME = 'Персонаж'
 
 export default function CharacterBuilder({ data }: { data: BuilderData }) {
 	// Character data
-	const [name, setName] = useState<string>('Персонаж')
+	const [name, setName] = useState<string>(DEFAULT_NAME)
+	const [classes, setClasses] = useState<DNDClass[]>([])
 
-	const [classes, setClasses] = useState<Set<DNDClass>>(new Set())
 	const addClass = (clazz: DNDClass) => {
-		setClasses(prev => new Set(prev).add(clazz))
+		setClasses(prev => {
+			if (!prev.includes(clazz)) return [...prev, clazz]
+			return prev
+		})
+
+		onClassSelection()
 	}
 	const removeClass = (clazz: DNDClass) => {
 		setClasses(prev => {
-			const updated = new Set(prev)
-			updated.delete(clazz)
-			return updated
+			return prev.filter(c => c !== clazz)
 		})
 	}
+	const onClassSelection = () => {
+		if (step === BuilderStep.CLASS_SELECTION) setStep(step + 1)
+	}
 
+	const [nextButtonActivity, setNextButtonActivity] = useState(true)
+	const [prevButtonActivity, setPrevButtonActivity] = useState(true)
+	const [endButtonActivity, setEndButtonActivity] = useState(false)
+	const [buttonErrorMsg, setErrorMsg] = useState('')
 	const [step, setStep] = useState<BuilderStep>(BuilderStep.LESSON_WELCOME)
+
+	useEffect(() => {
+		switch (step) {
+			case BuilderStep.LESSON_WELCOME:
+				setNextButtonActivity(true)
+				setPrevButtonActivity(true)
+				setEndButtonActivity(false)
+				setErrorMsg('Рано для завершения!')
+				break
+			case BuilderStep.CLASS_WELCOME:
+				setNextButtonActivity(true)
+				setPrevButtonActivity(true)
+				setEndButtonActivity(false)
+				setErrorMsg('Рано для завершения!')
+				break
+			case BuilderStep.CLASS_SELECTION:
+				if (classes.length > 0) setNextButtonActivity(true)
+				else setNextButtonActivity(false)
+				setPrevButtonActivity(true)
+				setEndButtonActivity(false)
+				setErrorMsg('Нужно выбрать класс!')
+				break
+		}
+	}, [step, classes])
 
 	const renderStep = () => {
 		switch (step) {
@@ -89,22 +123,28 @@ export default function CharacterBuilder({ data }: { data: BuilderData }) {
 					<BuilderButton
 						id='prevStep'
 						name='❮  Назад'
+						activity={prevButtonActivity}
 						onClick={() => setStep(step - 1)}
+						errorMsg={buttonErrorMsg}
 					/>
 				) : undefined}
 				{step < BuilderStep.END ? (
 					<BuilderButton
 						id='nextStep'
 						name='Далее  ❯'
+						activity={nextButtonActivity}
 						onClick={() => setStep(step + 1)}
+						errorMsg={buttonErrorMsg}
 					/>
 				) : (
 					<BuilderButton
 						id='endStep'
 						name='Завершить ✓'
+						activity={endButtonActivity}
 						onClick={() =>
 							console.error('TODO: ВЫВОДИТЬ ЭТО В PDF И СОХРАНЯТЬ ПОЛЬЗОВАТЕЛЮ')
 						}
+						errorMsg={buttonErrorMsg}
 					/>
 				)}
 			</ButtonsContainer>
