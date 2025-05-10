91e8fcf8a82a17609dbef3157e98ab104c99292a
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-10 21:59:36 +0300

    add: TextSection component

---
+++ src/components/CharacterLessons/TextSection.tsx
@@ -0,0 +1,67 @@
+import { TextData } from '@/types/CharacterLesson/Steps/TextData'
+import sanitizeHtml from 'sanitize-html'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-self: flex-start;
+	width: 100%;
+`
+
+const Title = styled.h3`
+	font-size: 1.5rem;
+	font-weight: 600;
+	margin-bottom: 10px;
+
+	@media (max-width: 2560px) {
+		font-size: 2.3rem;
+	}
+
+	@media (max-width: 1920px) {
+		font-size: 1.5rem;
+	}
+
+	@media (max-width: 1280px) {
+		font-size: 1.3rem;
+	}
+
+	@media (max-width: 1024px) {
+		font-size: 1.2rem;
+	}
+`
+
+const Text = styled.span`
+	font-size: 1rem;
+	font-weight: 400;
+
+	@media (max-width: 2560px) {
+		font-size: 1.7rem;
+	}
+
+	@media (max-width: 1920px) {
+		font-size: 1rem;
+	}
+
+	@media (max-width: 1280px) {
+		font-size: 1rem;
+	}
+
+	@media (max-width: 1024px) {
+		font-size: 0.9rem;
+	}
+`
+
+export type Props = {
+	data: TextData
+}
+
+export default function TextSection({ data }: Props) {
+	return (
+		<MainContainer>
+			<Title>{data.title}</Title>
+			<Text dangerouslySetInnerHTML={{ __html: sanitizeHtml(data.text) }} />
+		</MainContainer>
+	)
+}
