cac57a4462b4da10a934f67795a46a33817bd254
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-18 13:15:44 +0300

    add: bonusMastery to DNDCharacter

--- src/InteractiveLessons/Entities/Character/DND/DNDCharacter.ts
+++ src/InteractiveLessons/Entities/Character/DND/DNDCharacter.ts
@@ -77,6 +77,38 @@ export default class DNDCharacter extends Character {
 		else return (weapon as DNDWeaponData).maxRange
 	}
 
+	public get bonusMastery(): number {
+		switch (this._data.level) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				return 2
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+				return 3
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+				return 4
+			case 13:
+			case 14:
+			case 15:
+			case 16:
+				return 5
+			case 17:
+			case 18:
+			case 19:
+			case 20:
+				return 6
+		}
+
+		return 0
+	}
+
 	public get attackModifier(): number {
 		const weapon = this.equipmentManager.slotItem(
 			DNDEquipmentSlotType.MAIN_HAND
