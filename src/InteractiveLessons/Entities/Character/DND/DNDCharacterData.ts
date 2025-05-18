4eff933ed1e815101dc2bad9f27cfb962ffe33d8
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-18 13:16:10 +0300

    update: DNDCharacterData

--- src/InteractiveLessons/Entities/Character/DND/DNDCharacterData.ts
+++ src/InteractiveLessons/Entities/Character/DND/DNDCharacterData.ts
@@ -1,9 +1,12 @@
 import { DNDItemData } from '@/InteractiveLessons/EquipmentManager/DND/DNDItemData'
 import { DNDStatType } from '@/InteractiveLessons/StatsManager/DNDStatType'
+import { DNDClass } from './DNDClass'
 
 export type DNDCharacterData = {
 	type: 'DND'
 	name: string
+	class: DNDClass
+	level: number
 	maxHealth: number
 	maxMovementSpeed: number
 	maxSpellSlots: Map<number, number>
---
+++ src/InteractiveLessons/Entities/Character/DND/DNDClass.ts
@@ -0,0 +1,11 @@
+export enum DNDClass {
+	WIZARD = 'Wizard',
+	ARTIFICER = 'Artificer',
+	DRUID = 'Druid',
+	CLERIC = 'Cleric',
+	RANGER = 'Ranger',
+	SORCERER = 'Sorcerer',
+	WARLOCK = 'Warlock',
+	BARD = 'Bard',
+	PALADIN = 'Paladin',
+}
