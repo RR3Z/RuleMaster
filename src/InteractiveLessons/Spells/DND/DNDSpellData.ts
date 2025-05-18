b3012f4b8fe9cb3115997a725b25a9ed1cdcab61
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-18 14:06:34 +0300

    update: DNDSpellData

--- src/InteractiveLessons/Spells/DND/DNDSpellData.ts
+++ src/InteractiveLessons/Spells/DND/DNDSpellData.ts
@@ -1,4 +1,5 @@
 import { DiceRollerFormula } from '@/InteractiveLessons/DiceRoller/Types/DiceRollerFormula'
+import { DNDStatType } from '@/InteractiveLessons/StatsManager/DNDStatType'
 import { DNDRollType } from './DNDRollType'
 import { DNDSpellForm } from './DNDSpellForm'
 import { DNDSpellType } from './DNDSpellType'
@@ -9,6 +10,7 @@ export type DNDSpellData = {
 	radius: number
 	maxDistance: number
 	rollType: DNDRollType
+	savingThrowStat?: DNDStatType
 	rollsFormula: DiceRollerFormula[] | null
 	isAttachedToCharacter: boolean
 }
