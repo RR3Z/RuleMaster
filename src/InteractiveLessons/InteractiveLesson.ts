2d7b5c850abb5fc3df5339391dd5ea5e545ba137
Egor <104233247+RR3Z@users.noreply.github.com>
2025-05-21 16:53:35 +0300

    add: InteractiveLesson class

---
+++ src/InteractiveLessons/InteractiveLesson.ts
@@ -0,0 +1,16 @@
+import { Object3D } from 'three'
+import DiceRoller from './DiceRoller/DiceRoller'
+import { DiceType } from './DiceRoller/Types/DiceType'
+import { Game } from './Types/Game'
+
+export default class InteractiveLesson {
+	private _diceRoller!: DiceRoller
+
+	constructor(game: Game, dicesVisual: Map<DiceType, Object3D>) {
+		this._diceRoller = new DiceRoller(dicesVisual)
+	}
+
+	public get diceRollerCanvas(): HTMLCanvasElement {
+		return this._diceRoller.canvas
+	}
+}
